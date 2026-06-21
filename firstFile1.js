3be34c78118f44f99b29083cbd3b97d3ded29d99
agarwalkrishna4353-pro <agarwalkrishna4353@gmail.com>
2026-06-21 14:52:53 +0530

    new file add

--- firstFile1.js
+++ firstFile1.js
@@ -1,2 +1,21 @@
 let name = ["krishna", "sai", "vishnu"];
 console.log(name);
+let uesr_input_number = +prompt ("enter the number");
+if (uesr_input_number === 0 ){
+    console.log(`Today is Sunday day${uesr_input_number}`);
+} if (uesr_input_number === 1 ){
+    console.log(`Today is Monday day${uesr_input_number}`);
+} if (uesr_input_number === 2 ){
+    console.log(`Today is Tuesday day${uesr_input_number}`);
+} if (uesr_input_number === 3 ){
+    console.log(`Today is Wednesday day${uesr_input_number}`);
+} if (uesr_input_number === 4 ){
+    console.log(`Today is Thursday day${uesr_input_number}`);
+} if (uesr_input_number === 5 ){
+    console.log(`Today is Friday day${uesr_input_number}`);
+} if (uesr_input_number === 6 ){
+    console.log(`Today is Saturday day${uesr_input_number}`);
+}   else {
+    console.log("enter the valid number");
+}
+console.log(typeof uesr_input_number);
