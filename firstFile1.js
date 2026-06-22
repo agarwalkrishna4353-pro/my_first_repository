b3e53a345e26e3c1459c609bf9051c482c180f54
agarwalkrishna4353-pro <agarwalkrishna4353@gmail.com>
2026-06-22 23:06:36 +0530

    new file add

--- firstFile1.js
+++ firstFile1.js
@@ -3,17 +3,17 @@ console.log(name);
 let uesr_input_number = +prompt ("enter the number");
 if (uesr_input_number === 0 ){
     console.log(`Today is Sunday day${uesr_input_number}`);
-} if (uesr_input_number === 1 ){
+} else if (uesr_input_number === 1 ){
     console.log(`Today is Monday day${uesr_input_number}`);
-} if (uesr_input_number === 2 ){
+} else if (uesr_input_number === 2 ){
     console.log(`Today is Tuesday day${uesr_input_number}`);
-} if (uesr_input_number === 3 ){
+} else  if (uesr_input_number === 3 ){
     console.log(`Today is Wednesday day${uesr_input_number}`);
-} if (uesr_input_number === 4 ){
+} else if (uesr_input_number === 4 ){
     console.log(`Today is Thursday day${uesr_input_number}`);
-} if (uesr_input_number === 5 ){
+} else if (uesr_input_number === 5 ){
     console.log(`Today is Friday day${uesr_input_number}`);
-} if (uesr_input_number === 6 ){
+} else if (uesr_input_number === 6 ){
     console.log(`Today is Saturday day${uesr_input_number}`);
 }   else {
     console.log("enter the valid number");
